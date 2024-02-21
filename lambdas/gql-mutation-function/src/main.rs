f882aa4c5f82fb811a25e057870a75bd4bb2133d
hirohata <hirohatatro@gmail.com>
2024-02-21 15:24:40 -0800

    updated mutation function

--- lambdas/gql-mutation-function/src/main.rs
+++ lambdas/gql-mutation-function/src/main.rs
@@ -1,25 +1,38 @@
-use tracing_subscriber::filter::{EnvFilter, LevelFilter};use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
+use chrono::Local;
+use tracing_subscriber::filter::{EnvFilter, LevelFilter};
+use lambda_runtime::{run, service_fn, Error, LambdaEvent};
+use serde_json::{json, Value};
+
 
 /// This is the main body for the function.
 /// Write your code inside it.
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
+/// - https://github.com/aws-samples/serverless-rust-demo/
+async fn function_handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
     // Extract some useful information from the request
-    let who = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
+    println!("{:?}", event.payload);
 
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type", "text/html")
-        .body(message.into())
-        .map_err(Box::new)?;
-    Ok(resp)
+    let id = match &event.payload["arguments"]["input"]["id"].is_string() {
+        true => event.payload["arguments"]["input"]["id"].as_str(),
+        false => Some("1")
+    };
+
+    let description = &event.payload["arguments"]["input"]["description"];
+
+    if !description.is_string() {
+        panic!("description is not provided")
+    }
+
+    let datetime = Local::now().to_rfc3339();
+
+    let return_val = json!({
+        "id": id.unwrap_or("1"),
+        "description": description,
+        "datetime": datetime
+    });
+
+    Ok(return_val)
 }
 
 #[tokio::main]
