0c249a67accc0eae27c50c7f7fa2d24547bc0a3d
hirohata <hirohatatro@gmail.com>
2024-02-21 12:56:53 -0800

    update

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,74 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {UserPool} from "aws-cdk-lib/aws-cognito";
+import * as appsync from "aws-cdk-lib/aws-appsync"
+import * as path from "path";
+import { RustFunction } from 'cargo-lambda-cdk'
+import {IFunction} from "aws-cdk-lib/aws-lambda";
+import {GraphqlApi} from "aws-cdk-lib/aws-appsync";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const userPool = this.userPool();
+    const pubFunction = this.pubFunction();
+    const gqlMutationFunction = this.gqlMutationFunction();
+    const api = this.appSync(userPool);
+    this.attachResolver(gqlMutationFunction, api);
+  }
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  userPool() {
+    return new UserPool(this, 'userPool', {
+      selfSignUpEnabled: true,
+      signInAliases: {
+        email: true
+      }
+    })
+  }
+
+  gqlMutationFunction() {
+    return new RustFunction(this, 'gqlMutationFunction', {
+      manifestPath: path.join(__dirname, '../../lambdas/gql-mutation-function/Cargo.toml'),
+      runtime: 'provided.al2023'
+    })
+  }
+
+  pubFunction() {
+    return new RustFunction(this, 'pubFunction', {
+      manifestPath: path.join(__dirname, '../../lambdas/pub-function/Cargo.toml'),
+      runtime: 'provided.al2023'
+    })
+  }
+
+  appSync(userPool: UserPool) {
+    return new appsync.GraphqlApi(this, 'api', {
+      name: "pub-sub",
+      definition: appsync.Definition.fromFile(path.join(__dirname, '../../graphql/schema.graphql')),
+      // authorizationConfig: {
+      //   defaultAuthorization: {
+      //
+      //   }
+      // }
+    })
+  }
+
+  attachResolver(lambdaResolverFunction: IFunction, appSync: GraphqlApi) {
+
+    const lambdaDataSource = appSync.addLambdaDataSource('lambda-resolver', lambdaResolverFunction);
+
+    const fields: {typeName: string, fieldName: string}[] = [
+        {
+          typeName: 'Mutation',
+          fieldName: 'addDemo',
+        }
+    ]
+
+    for (const {typeName, fieldName}  of fields) {
+      lambdaDataSource.createResolver(`${typeName}-${fieldName}`, {
+        typeName,
+        fieldName,
+      })
+    }
   }
 }
